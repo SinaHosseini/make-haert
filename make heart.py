9c0e192ab6324d67cb9e984ac36eb667ffd44ce8
seyed sina hosseini vaez <122821690+SinaHosseini@users.noreply.github.com>
2023-02-27 21:39:50 +0330

    Add files via upload

---
+++ make heart.py
@@ -0,0 +1,17 @@
+from turtle import *
+from turtle import bgcolor, delay
+
+bgcolor('black')
+color('red')
+delay(10)
+begin_fill()
+pensize(5)
+left(50)
+forward(130)
+circle(50, 200)
+right(140)
+circle(50, 200)
+forward(130)
+end_fill()
+
+done()
